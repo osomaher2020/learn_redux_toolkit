3ad8ba5db9e44fed69e80c6cd1b11c43536f559b
Osama Maher <72032764+osomaher2020@users.noreply.github.com>
2023-05-12 06:09:33 +0000

    more explanation for [redux-thunk] @ 7_async_actions_thunk

--- 7_async_actions_thunk/store.js
+++ 7_async_actions_thunk/store.js
@@ -1,9 +1,29 @@
 const redux = require('redux')
 
-// Thunk: a middleware --> defines Async actionCreators    [yarn add redux-thunk]
+// "Thunk" means=>  "a piece of code that does some delayed work"
+// Thunk (middleware) --> the standard way to define --> "Async actionCreators"
+// "action creator" => function which dispatch some [ actions == "thunk functions"] :)
+// "thunk function" accepts (dispatch method, getState)   ----   can be called via:->  store.dispatch()
+// [yarn add redux-thunk]
 const thunk = require('redux-thunk').default
 
-const applyMiddleWare = redux.applyMiddleware
+// -- ex --:
+// fetchTodoById is the "thunk action creator"
+function fetchTodoById(todoId) {
+    // fetchTodoByIdThunk is the "thunk function" --> which will perform Async task --then--> dispatch(action)
+    return async function fetchTodoByIdThunk(dispatch, getState) {
+        const response = await client.get(`/fakeApi/todo/${todoId}`)
+        dispatch(todosLoaded(response.todos))
+    }
+}
+// same as === arrow function
+// const fetchTodoById = todoId => async dispatch => {
+//     const response = await client.get(`/fakeApi/todo/${todoId}`)
+//     dispatch(todosLoaded(response.todos))
+// }
+
+
+
 
 // 1- State
 const initialState = {
@@ -17,18 +37,18 @@ const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED'
 const FETCH_USERS_SUCCEDED = 'FETCH_USERS_SUCCEDED'
 const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED'
 
-const fetchUsersRequest = () => (
+const fetchUsersRequest = () => ( // every Action -returns-> {object}
     {
         type: FETCH_USERS_REQUESTED
     }
 )
-const fetchUsersSuccess = users => (
+const fetchUsersSuccess = users => ( // every Action -returns-> {object}
     {
         type: FETCH_USERS_SUCCEDED,
         payload: users
     }
 )
-const fetchUsersFail = error => (
+const fetchUsersFail = error => ( // every Action -returns-> {object}
     {
         type: FETCH_USERS_FAILED,
         payload: error
@@ -65,30 +85,37 @@ const reducer = (state = initialState, action) => {
 }
 
 
-// 4- Async Action_Creator
+// "thunk action creator"
+// will perform Async func() --then--> dispatch(action)
+// 4- Async Action_Creator <<<<<< will (Promise) to perform -> dispatch(ACTION) --depending on--> the Async response :)
 const fetchUsers = () => {
-    return function(dispatch){
+    return function (dispatch) {
         dispatch(fetchUsersRequest()) // sets loading:true
 
         // fetching from API
-        fetch(`https://jsonplaceholder.typicode.com/users`)
-        .then(res=> res.json())
-        .then(data => {
-            const users = data.map(({id}) => id)
-            dispatch(fetchUsersSuccess(users))
-        })
-        .catch(err => {
-            dispatch(fetchUsersFail(err.message))
-        })
+        fetch(`https://jsonplaceholder.typicode.com/users`) // <<< try to make url malformed << to see Error
+            .then(res => res.json())
+            .then(data => { // SUCCESS
+                const users = data.map(({ id }) => id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(err => { // FAIL
+                dispatch(fetchUsersFail(err.message))
+            })
     }
 }
 
 
 // 5- store
-const store = redux.createStore(reducer, applyMiddleWare(thunk))
+const store = redux.createStore(reducer, redux.applyMiddleware(thunk))
 
 
 // running
-store.subscribe(() => console.log(store.getState()))
+store.subscribe(() => console.log("--- Normal subscribe for tracking state --- \n\n", store.getState(), "\n\n"))
 
 store.dispatch(fetchUsers())
+// *** NOTE ***
+// fetchUsers() --> is not an action by itself --> but it creates the appropriate dispatch(ACTION)
+// * so it is called "Action Creator"
+
+// *** that is why we need to use 'redux-thunk' *** => to enables us to call the "Action Creator" function :D
